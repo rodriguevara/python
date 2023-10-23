d734fd8c27e36fc34250f702a9ac06b487ac5893
Rodrigo Ladron de Guevara <rodrigo.ladrondeguevara@edipro.cl>
2023-10-23 09:07:33 -0300

    final guia 7

--- guia7.py
+++ guia7.py
@@ -1,5 +1,6 @@
 import array
-
+import random
+import numpy as np
 s = [1,2,3,4]
 e = 6
 f = "F"
@@ -30,7 +31,7 @@ def pertenece3(s,e) -> bool:
             return True
     return False
 
-#print(pertenece3(["a","e","i","o","u"],"f"))    
+#print(pertenece3(["a","e","i","o","u"],"f"))
 
 #1.2
 def divideATodos(s:[int], e: int) -> bool:
@@ -58,8 +59,7 @@ def sumaTotal(s: [int]) -> int:
 
 def ordenados(s:list) -> bool:
     for i in range(0,len(s)-1):
-        if s[i] >= s[i+1]:
-            
+        if s[i] > s[i+1]:
             return False
     return True
 
@@ -94,7 +94,7 @@ def contraseña(s: str) -> str:
     res = ""
     if len(s) < 5:
         return "ROJA"
-    
+
     for i in range(0,len(s)):
         if pertenece3(abc,s[i]):
             cond1 = True
@@ -105,7 +105,7 @@ def contraseña(s: str) -> str:
     if cond1 == True and cond2 == True and cond3 == True and len(s) > 8:
         return "VERDE"
     return "AMARILLA"
-    
+
 #print(contraseña("Holaquetal1"))
 
 def contraseña2(s: str) -> str:
@@ -122,7 +122,7 @@ def contraseña2(s: str) -> str:
             cond2 = True
         if s[i] <= "9" and s[i] >= "0":
             cond3 = True
-    
+
     if cond1 == True and cond2 == True and cond3 == True and len(s) > 8:
         res = "VERDE"
     elif len(s) < 5:
@@ -184,7 +184,7 @@ def paresBorraNuevaLista(s:list) -> list:
     return newList
 
 #print(paresBorraNuevaLista([1,2,3,4,5]))
-
+# 2.3
 def sinVocales(s: str) -> str:
     vocs = ["a","e","i","o","u"]
     newStr = ""
@@ -193,4 +193,142 @@ def sinVocales(s: str) -> str:
             newStr += s[i]
     return newStr
 
-print(sinVocales("Holaquetal"))
+# print(sinVocales("Holaquetal"))
+
+# 2.4
+def reemplazaVocales(s: list) -> list:
+    vocs = ["a","e","i","o","u"]
+    newStr = ""
+    for i in range(0,len(s)):
+        if not pertenece3(vocs,s[i]):
+            newStr += s[i]
+        else:
+            newStr += '-'
+    return newStr
+# print(reemplazaVocales("Holaquetal"))
+
+# 2.5
+
+def daVueltaString(s: str) -> str:
+    newStr = ""
+    for i in range(0,len(s)):
+        newStr += s[-1-i]
+    return newStr
+
+# print(daVueltaString("Holaquetal"))
+
+# 2.6
+def eliminarRepetidos(s:str) -> str:
+    newStr = ''
+    for i in range(0,len(s)):
+        if not pertenece3(newStr,s[i]):
+            newStr += s[i]
+    return newStr
+# print(eliminarRepetidos('menemer'))
+
+# 3
+def aprobado(s: list) -> int:
+    res = 0
+    promedio = sumaTotal(s)/len(s)
+
+    if promedio < 4 or menorA4(s) == True:
+        res = 3
+    elif promedio < 7:
+        res = 2
+    else:
+        res = 1
+    return res
+
+
+def menorA4(s: list) -> bool:
+    for i in range(0,len(s)):
+        if s[i] < 4:
+            return True
+    return False
+
+# print(aprobado([10,4,-4]))
+# 4.1
+def nombresEstudiantes() -> list:
+    res = []
+    palabraEscape = 'listo'
+    estudiante: str = input('ingrese el nombre del estudiante: ')
+    while estudiante != 'listo':
+        res.append(estudiante)
+        estudiante: str = input('ingrese el nombre del estudiante: ')
+    return res
+# print(nombresEstudiantes())
+
+# 4.2
+def historial() -> list:
+    accion: str = input('Ingrese la accion: C para cargar, D para descontar, X para terminar: ')
+    total: int = 0
+    res = []
+    while accion != 'X':
+        monto: int = input('Ingrese la cantidad: ')
+        if accion == 'C':
+            print(monto)
+            total += int(monto)
+            res.append([accion,monto,total])
+        elif accion == 'D':
+            total -= int(monto)
+            res.append([accion,monto,total])
+        accion: str = input('Ingrese una nueva accion: C para cargar, D para descontar, X para terminar: ')
+    return res
+
+# print(historial())
+
+# 4.3
+def juego() -> str:
+    numsjuego = [1,2,3,4,5,6,7,10,11,12]
+    accion = input('Enter para pedir una carta: ')
+    cuenta = 0
+    while accion != 'plantarse':
+        paso = random.choice(numsjuego)
+        if paso > 9:
+            cuenta += 0.5
+        else:
+            cuenta += paso
+        if cuenta > 7.5:
+            print(cuenta)
+            return 'perdiste'
+        print(cuenta)
+        accion: str = input('Ingrese plantarse para finalizar o enter para pedir una carta: ')
+    return cuenta
+# print(juego())
+# 5.1
+def perteneceACadaUno(s: [[int]], e: int) -> [bool]:
+    res = []
+    for i in range(0,len(s)):
+        if pertenece3(s[i],e):
+            res.append(True)
+        else:
+            res.append(False)
+    return res
+
+# print(perteneceACadaUno([[1,2,3,5],[3,4,5]],5))
+# 5.2
+def esMatriz(s: [[int]]) -> bool:
+    for i in range(1,len(s)):
+        if len(s[i]) != len(s[0]):
+            return False
+    return True
+
+# print(esMatriz([[0,1,2],[3,4]]))
+
+# 5.3
+def filasOrdenadas(s: [[int]]) -> [bool]:
+    res = []
+    for i in range(0,len(s)):
+        if ordenados(s[i]):
+            res.append(True)
+        else:
+            res.append(False)
+    return res
+
+# print(filasOrdenadas([[0,1,2],[3,3,4,4]]))
+# 5.4
+def potenciaMatriz(d: int, p: int) -> list:
+    m = np.random.random((d, d))**2
+    p = np.linalg.matrix_power(np.array(m),p)
+    return p
+# print(potenciaMatriz(5,3))
